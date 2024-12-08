980c6a901f5baf0c00c34957082282ab0dac4188
Sparshagarwal29 <sparshagarwal2909@gmail.com>
2024-12-08 22:54:01 +0530

    Save data and genrate html for payment

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,5 +1,5 @@
-import {renderOrderSummary} from './chekout/orderSummary.js';
-import {renderPaymentSummary} from './chekout/paymentSumary.js';
+import {renderOrderSummary} from './checkout/orderSummary.js';
+import {renderPaymentSummary} from './checkout/paymentSummary.js';
 
 renderOrderSummary();
 renderPaymentSummary();
